69a711886716ea4f6e769ae49d674e9f7f62eb5b
Steeve <steevechailloux@gmail.com>
2014-10-03 16:54:58 +0200

    change license to MIT, update setup

--- setup.py
+++ setup.py
@@ -2,10 +2,20 @@
 from setuptools import setup, find_packages
 
 
+try:
+    import pypandoc
+    description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    with open('README.md', 'rb') as f:
+        description = f.read()
+
+
 setup(
     name='djangocms-css-background',
-    version='0.0.1',
+    version='1.0.1b',
+    license='MIT',
     description='A django-cms plugin which allow you to edit css background image or color from the edit mode',
+    long_description=description,
     author='Steeve',
     author_email='mo0ofier@gmail.com',
     include_package_data=True,
@@ -15,9 +25,10 @@ setup(
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
     ],
     zip_safe=False,
 )
